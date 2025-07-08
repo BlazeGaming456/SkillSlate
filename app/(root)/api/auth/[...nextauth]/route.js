78e961ed9d7935458c5131681bac8b16a423742b
unknown <ajincraju@gmail.com>
2025-07-08 19:08:15 +0530

    Fixed Deployment Error - III

--- app/(root)/api/auth/[...nextauth]/route.js
+++ app/(root)/api/auth/[...nextauth]/route.js
@@ -1,8 +1,20 @@
 //Creates a NextAuth handler for Google authentication
 
 import NextAuth from "next-auth";
-import { authOptions } from "@/components/auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      return session;
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
